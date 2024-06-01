f4d61630bbbc9d33abfe4b0ef31b022b5b8d1518
PriyomDatta <priyomdat@gmail.com>
2024-06-02 03:30:43 +0530

    NodeMCU Connect to Hotspot of Mobile

---
+++ ESP8266/Connect_to_WiFi/Connect_to_WiFi.ino
@@ -0,0 +1,25 @@
+/*
+Author:-priyom Datta
+Date:-06/02/24
+Note:- Select generic ESP8266 Module
+*/
+#include<ESP8266WiFi.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("Priyom's phone","Priyom123");
+  while(WiFi.status()!=WL_CONNECTED)
+  {
+    Serial.print("..");
+    delay(500);
+  }
+  Serial.println("");
+  Serial.println("NodeMCU is connected");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
